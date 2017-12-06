ae31f20cacef661a6bc22c438d20231e78a6a5b8
badcock4412 <benadcock@gmail.com>
2017-12-06 17:14:33 -0500

    Changed name to index.html

--- geosw.js
+++ geosw.js
@@ -2,7 +2,8 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/geocapture/geolocation_capture.html',
+	    '/geocapture/',
+        '/geocapture/index.html',
       ]);
     })
   );
